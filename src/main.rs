a3e636b42342ba877c886edb2567e9e48f04878c
King Claudy <clocloranely@hotmail.fr>
2017-08-24 22:49:47 +0200

    Version fonctionnelle de l'ajout de SetCookie
    TODO : La requête demande par contre d'avoir le champ `Cookie` rempli et non `SetCookie`

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::io::BufReader;
 use std::fs::File;
 use std::thread;
 use std::sync::mpsc::channel;
-use reqwest::header::{Headers, Cookie, SetCookie};
+use reqwest::header::{Cookie, Headers, SetCookie};
 use scraper::{Html, Selector};
 use std::str;
 use watcher::*;
@@ -44,61 +44,64 @@ fn credentials_login() -> Config {
 
 /// Retrieve the SetCookie for the rest of the HTTP transactions
 
-fn request_sequence(c: Config) {
+fn request_sequence(c: Config) -> String {
     // Login in using credentials
     let client = reqwest::Client::new().expect("Initialization of the client failed");
-    let uri: String = format!("https://{}/{}", &c.base_uri, &c.login_uri).parse().unwrap();
+    let uri: String = format!("https://{}/{}", &c.base_uri, &c.login_uri)
+        .parse()
+        .unwrap();
     let params = [("username", &c.username), ("password", &c.password)];
-    let post_request = client.post(&uri)
+    let post_request = client
+        .post(&uri)
         .expect("POST failed")
         .form(&params)
         .expect("Wraping of the forms parameters failed")
         .send()
-        .expect("POST request failed to be sent");
-    let uri_grade: String = format!("https://{}/{}", &c.base_uri, &c.grade_uri).parse().unwrap();
+        .expect("POST request failed to be sent"); 
+    let uri_grade: String = format!("https://{}/{}", &c.base_uri, &c.grade_uri)
+        .parse()
+        .unwrap();
     let mut setcookie: Vec<String> = Vec::new();
-    if let Some(&SetCookie(content)) = post_request.headers().get::<SetCookie>() {
-        setcookie = content.clonpe();
+    if let Some(&SetCookie(ref content)) = post_request.headers().get::<SetCookie>() {
+        setcookie = content.clone();
     }
     let mut headers = Headers::new();
-    let cookie = Cookie::new();
-    print!("{:?}", setcookie);
-    //  cookie.set(setcookie);
-    // headers.set(cookie);
-    // print!("{:?}",
-    // client.get(&uri_grade)
-    // .expect("GET request failed")
-    // .header(headers)
-    // .send()
-    // .expect("GET request failed to be sent"));
+    headers.set(SetCookie(setcookie));
+    let mut html_page_content = String::new();
+    client
+        .get(&uri_grade)
+        .expect("GET request failed")
+        .headers(headers)
+        .send()
+        .expect("GET request failed to be sent")
+        .read_to_string(&mut html_page_content);
+
+    html_page_content
 }
 
 /// Retrieve a vector of numbers corresponding to the IDs of all the courses
 
-// fn retreive_all_courses_id(c: Config) {
-// let mut result_from_get = request_sequence(c);
-// let mut result_from_get = String::new();
-// let html_page_content = str::from_utf8_mut(&mut result_from_get).unwrap();
-// Creating the parsed html page
-// let html_page_content = Html::parse_document(&html_page_content);
-// println!("{:?}",html_page_content);
-// Create the parser
-// let selector = Selector::parse("overview-grade").expect("Initializing the parsing failed");
-// println!(" Parsed : {:?}", selector);
-// Parsing the html to find the table
-// let grade_table = html_page_content.select(&selector)
-// .collect::<Vec<_>>()
-// .iter()
-// .map(|&x| x.inner_html())
-// .collect::<String>();
-// println!("{:?}", grade_table);
-// }
+fn retreive_all_courses_id(c: Config) {
+    let html_page_content = request_sequence(c);
+
+    // Creating the parsed html page
+    let html_page_content = Html::parse_document(&html_page_content);
+    // Create the parser
+    let selector = Selector::parse("overview-grade").expect("Initializing the parsing failed");
+    // Parsing the html to find the table
+    let grade_table = html_page_content
+        .select(&selector)
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|&x| x.inner_html())
+        .collect::<String>();
+    println!("Table : {:?}", grade_table);
+}
 
 fn main() {
     let c = credentials_login();
     // Obtaining all the courses
-    request_sequence(c);
-    // retreive_all_courses_id(c);
+    retreive_all_courses_id(c);
     // let coursesID: Vec<i32> = retreive_all_courses_id(c); // Array containing the courses ID
 
     // for nc in coursesID {
